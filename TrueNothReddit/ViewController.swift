4365756478f8df339694905e4ae2eb17e48ab34d
Manu Alvarez <alvarezmanuelignacio@gmail.com>
2022-01-20 19:57:45 -0300

    init proyect

---
+++ TrueNothReddit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrueNothReddit
+//
+//  Created by Manuel Alvarez on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
